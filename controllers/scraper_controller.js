e54eb06a4297c223e91d663a931342376ebdc1f1
David Lawrence <dalaw85@gmail.com>
2019-05-29 13:20:49 -0400

    note post route working

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -1,11 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
-
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-// Require all models
 var db = require("../models");
 
 // var Article = require("../models/index.js");
@@ -49,51 +45,70 @@ router.get("/scrape", function(req, res) {
     });
   });
   
-  router.get("/", function(req, res) {
-    // Grab every document in the Articles collection
+router.get("/", function(req, res) {
+// Grab every document in the Articles collection
     db.Article.find({})
-      .then(function(data) {
+        .then(function(data) {
         // If we were able to successfully find Articles, send them back to the client
 
         var hbsObject = {
             articles: data
-          };
-          console.log(hbsObject);
-          res.render("index", hbsObject);
-      })
-      .catch(function(err) {
+            };
+            console.log(hbsObject);
+            res.render("index", hbsObject);
+        })
+        .catch(function(err) {
         // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+            res.json(err);
+        });
+});
 
-  router.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
+router.get("/articles", function(req, res) {
+// Grab every document in the Articles collection
     db.Article.find({})
-      .then(function(dbArticle) {
+        .then(function(dbArticle) {
 
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
         // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  
-  // Route for grabbing a specific Article by id, populate it with it's note
-  router.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+            res.json(err);
+        });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function(req, res) {
+// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+    // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
+    })
+    .catch(function(err) {
+    // If an error occurred, send it to the client
         res.json(err);
-      });
-  });
+    });
+});
+
+router.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
   module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,70 +1,60 @@
-// // Grab the articles as a json
-// $.getJSON("/articles", function(data) {
-//     // For each one
-//     for (var i = 0; i < data.length; i++) {
-//       // Display the apropos information on the page
-//       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-//     }
-//   });
-  
-  
-  // Whenever someone clicks a p tag
-  $(".summary").on("click", function() {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-  
-    // Now make an ajax call for the Article
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#notes").append("<input id='name-input' name='name' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='body-input' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        if (data.note) {
-          $("#name-input").val(data.note.name);
-          $("#body-input").val(data.note.body);
-        }
-      });
-  });
+    // Whenever someone clicks a p tag
+$(document).on("click", ".summary", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
 
-  $(document).on("click", "#save-note", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        name: $("#name-input").val(),
-        // Value taken from note textarea
-        body: $("#body-input").val()
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.name + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='name-input' name='name' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='body-input' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='save-note'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        $("#name-input").val(data.note.name);
+        $("#body-input").val(data.note.body);
       }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#name-input").val("");
-    $("#body-input").val("");
-  });
-  
+    });
+});
+
+$(document).on("click", "#save-note", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  var newNote = {
+    name: $("#name-input").val(),
+    // Value taken from note textarea
+    body: $("#body-input").val()
+  };
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: newNote
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#name-input").val("");
+  $("#body-input").val("");
+});
