ac728dddcc271ce9749cbf94a08d5dd238ee19ab
David Lawrence <dalaw85@gmail.com>
2019-06-04 12:28:27 -0400

    server db connect

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.use(routes);
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/Scraper", { useNewUrlParser: true });
 
-// var MONGODB_URI =  ("mongodb://localhost/Scraper", { useeNewUrlParser: true});
-// // "mongodb://heroku_0f6n41sd:JuAUE7AV2LLpbWg@ds131747.mlab.com:31747/heroku_0f6n41sd" ||
-// mongoose.connect(MONGODB_URI);
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = "mongodb://heroku_0f6n41sd:JuAUE7AV2LLpbWg@ds131747.mlab.com:31747/heroku_0f6n41sd" || ("mongodb://localhost/Scraper", { useeNewUrlParser: true});
 
 mongoose.connect(MONGODB_URI);
+
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+// mongoose.connect(MONGODB_URI);
 
 // Start the server
 app.listen(PORT, function() {
